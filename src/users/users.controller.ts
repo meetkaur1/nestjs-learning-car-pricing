f9793658a6e81c48ca3bda8bc2a3eb55bd48174b
Harmeet Kaur <hk.harmeetkaur2017@gmail.com>
2025-09-26 17:55:31 +0530

    feat(interceptor): add SerializeInterceptor using class-transformer for DTO response mapping

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -2,8 +2,11 @@ import { Body, Controller, Delete, Get, NotFoundException, Param, Patch, Post, Q
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UsersService } from './users.service';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { Serialize } from 'src/interceptors/serialize.interceptor';
+import { UserDto } from './dtos/user.dto';
 
 @Controller('auth')
+@Serialize(UserDto)
 export class UsersController {
     constructor(private usersService: UsersService) { }
 
